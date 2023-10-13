ab0b81fa31c6bfc3b95debbcb924755ccf442e6f
couldBeFree <zoriaalexandr@gmail.com>
2023-10-13 16:32:29 +0300

    implemented creating user

--- src/repository/user_repo.rs
+++ src/repository/user_repo.rs
@@ -1,13 +1,13 @@
 extern crate dotenv;
 
-// use futures::TryStreamExt;
-// use mongodb::{
-//     bson::{extjson::de::Error, oid::ObjectId, doc},
-//     results::{ InsertOneResult, UpdateResult, DeleteResult },
-//     Collection, Database
-// };
+use std::vec;
 
-use mongodb::{Collection, Database};
+use futures::TryStreamExt;
+use mongodb::{
+    bson::{extjson::de::Error, oid::ObjectId, doc},
+    results::{ InsertOneResult, UpdateResult, DeleteResult },
+    Collection, Database
+};
 
 use crate::models::user_model::User;
 
@@ -20,5 +20,23 @@ impl UserRepo {
     pub async fn init(db: &Database) -> Self {
         let collection: Collection<User> = db.collection("user");
         UserRepo { collection }
+    }
+
+    pub async fn create_user(&self, new_user: User) -> Result<InsertOneResult, Error> {
+        let new_user = User {
+            id: None,
+            name: new_user.name.to_owned(),
+            email: new_user.email.to_owned(),
+            password: new_user.password.to_owned(),
+            expenses: Some(vec![]),
+            incomes: Some(vec![])
+        };
+        let user = self
+            .collection
+            .insert_one(new_user, None)
+            .await
+            .ok()
+            .expect("Error creating user");
+        Ok(user)
     }
 }
--- src/views/auth/register.rs
+++ src/views/auth/register.rs
@@ -1,5 +1,20 @@
-use actix_web::{Responder, HttpResponse};
+use crate::{models::user_model::User, app_state::app_state::AppState};
+use actix_web::{Responder, HttpResponse, web::Data, web::{Json, Path}};
 
-pub async fn register() -> impl Responder {
-    return HttpResponse::Ok()
+pub async fn register(db: Data<AppState>, new_user: Json<User>) -> impl Responder {
+    let data = User {
+        id: None,
+        name: new_user.name.to_owned(),
+        email: new_user.email.to_owned(),
+        password: new_user.password.to_owned(),
+        expenses: Some(vec![]),
+        incomes: Some(vec![])
+    };
+    let result = db.user_repo.create_user(data).await;
+    match result {
+        Ok(user) => HttpResponse::Ok().json(user),
+        Err(err) => {
+            HttpResponse::BadRequest().json(err.to_string())
+        }
+    }
 }
